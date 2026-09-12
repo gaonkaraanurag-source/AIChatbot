1bb07a083d85c23caed6a04c0312e6e227416a56
Anurag Gaonkar <gaonkaraanurag@gmail.com>
2026-09-12 19:19:21 -0400

    Selected groq model for bot

--- CLIChatbot.py
+++ CLIChatbot.py
@@ -7,3 +7,15 @@ if "GROQ_API_KEY" not in os.environ:
 if "LANGSMITH_API_KEY" not in os.environ:
     os.environ["LANGSMITH_API_KEY"] = getpass.getpass("Enter your LangSmith API key: ")
     os.environ["LANGSMITH_TRACING"] = "true"
+
+from langchain_groq import ChatGroq
+
+llm = ChatGroq(
+    model="openai/gpt-oss-20b",
+    temperature=0,
+    max_tokens=None,
+    reasoning_format="parsed",
+    timeout=None,
+    max_retries=2,
+    # other params...
+)
