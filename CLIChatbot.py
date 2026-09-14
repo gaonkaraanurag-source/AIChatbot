f5e2eaf628e8695816e510c9d00146b9f9e73f98
Anurag Gaonkar <gaonkaraanurag@gmail.com>
2026-09-14 13:03:31 -0400

    added gorq model to the code

--- CLIChatbot.py
+++ CLIChatbot.py
@@ -19,3 +19,14 @@ llm = ChatGroq(
     max_retries=2,
     # other params...
 )
+
+messages = [
+    (
+        "system",
+        "You are a helpful assistant that translates English to French. Translate the user sentence.",
+    ),
+    ("human", "I love programming."),
+]
+ai_msg = llm.invoke(messages)
+ai_msg
+print(ai_msg.content)
